999196dbea16bab98ce5a962e36944e4e3f6a1fe
Paul Mascall <pjwmascall@hotmail.com>
2022-01-27 01:09:32 +0000

    written tests for calculator

--- modules/calculator.py
+++ modules/calculator.py
@@ -1,17 +1,70 @@
 class Calculator():
 
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+    def __init__(self):
+        self.num1 = None
+        self.num2 = None
+    
+    def number_is_integer(self, num):
+        return True if isinstance(num, int) else False
+
+    def number_is_float(self, num):
+        return True if isinstance(num, float) else False
+ 
+    def number_is_none(self, num):
+        return True if num == None else False
+
+    def number_is_string(self, num):
+        return True if isinstance(num, str) else False
+
+    def can_convert_to_float(self, num):
+        try:
+            return isinstance(float(num), float)
+        except ValueError:
+            return False
+        except TypeError:
+            return False
+
+    def can_use_number(self, num):
+        if self.number_is_integer(num):
+            return True
+        if self.number_is_float(num):
+            return True
+        if self.number_is_none(num):
+            return False
+        if self.number_is_string(num):
+            return self.can_convert_to_float(num)
+
+    def can_use_numbers(self, num1, num2):
+        return True if self.can_use_number(num1) and self.can_use_number(num2) else False
+
+    def number_is_zero(self, num):
+        return True if num == 0 else False
 
     def add(self, num1, num2):
-        return num1 + num2
+        if self.can_use_numbers(num1, num2):
+            return float(num1) + float(num2)
+        else:
+            return 'Error: Cannot add values'
 
     def subtract(self, num1, num2):
-        return num1 - num2
+        if self.can_use_numbers(num1, num2):
+            return float(num1) - float(num2)
+        else:
+            return 'Error: Cannot subtract values'
 
     def multiply(self, num1, num2):
-        return num1 * num2
+        if self.can_use_numbers(num1, num2):
+            return float(num1) * float(num2)
+        else:
+            return 'Error: Cannot multiply values'
 
     def divide(self, num1, num2):
-        return num1 / num2
+        if self.can_use_numbers(num1, num2):
+            num1 = float(num1)
+            num2 = float(num2)
+            if self.number_is_zero(num2):
+                return 'Error: Cannot divide by zero'
+            else:
+                return num1 / num2
+        else:
+            return 'Error: Cannot divide values'
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -4,7 +4,108 @@ from modules.calculator import Calculator
 class TestCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.pair1 = Calculator(1, 1)
+        self.calculator = Calculator()
 
-    def mytest(self):
-        self.assertEqual(True, True)
+    def test_has_first_number(self):
+        self.calculator.num1 = 1
+        self.assertEqual(1,self.calculator.num1)
+
+    def test_has_second_number(self):
+        self.calculator.num2 = 1
+        self.assertEqual(1, self.calculator.num2)
+
+    def test_number_is_integer_pass(self):
+        self.assertEqual(True, self.calculator.number_is_integer(1))
+
+    def test_number_is_integer_fail(self):
+        self.assertEqual(False, self.calculator.number_is_integer(1.0))
+
+    def test_number_is_float_pass(self):
+        self.assertEqual(True, self.calculator.number_is_float(1.0))
+
+    def test_number_is_float_fail(self):
+        self.assertEqual(False, self.calculator.number_is_float(1))
+
+    def test_number_is_none_pass(self):
+        self.assertEqual(True, self.calculator.number_is_none(None))
+
+    def test_number_is_none_fail(self):
+        self.assertEqual(False, self.calculator.number_is_none(1))
+
+    def test_number_is_string_pass(self):
+        self.assertEqual(True, self.calculator.number_is_string("1"))
+
+    def test_number_is_string_fail(self):
+        self.assertEqual(False, self.calculator.number_is_string(1))
+
+    def test_can_convert_to_float_pass_float(self):
+        self.assertEqual(True, self.calculator.can_convert_to_float(1.0))
+
+    def test_can_convert_to_float_pass_integer(self):
+        self.assertEqual(True, self.calculator.can_convert_to_float(1))
+
+    def test_can_convert_to_float_pass_string(self):
+        self.assertEqual(True, self.calculator.can_convert_to_float("1"))
+
+    def test_can_convert_to_float_fail_string_ValueError(self):
+        self.assertEqual(False, self.calculator.can_convert_to_float("one"))
+
+    def test_can_convert_to_float_fail_none_TypeError(self):
+        self.assertEqual(False, self.calculator.can_convert_to_float(None))
+
+    def test_can_use_number_pass(self):
+        self.assertEqual(True, self.calculator.can_use_number("1"))
+
+    def test_can_use_number_fail(self):
+        self.assertEqual(False, self.calculator.can_use_number("one"))
+
+    def test_can_use_numbers_pass(self):
+        self.assertEqual(True, self.calculator.can_use_numbers("1", 1))
+
+    def test_can_use_numbers_fail(self):
+        self.assertEqual(False, self.calculator.can_use_numbers(1.0, "one"))
+
+    def test_number_is_zero_pass(self):
+        self.assertEqual(True, self.calculator.number_is_zero(0.0))
+
+    def test_number_is_zero_fail(self):
+        self.assertEqual(False, self.calculator.number_is_zero(0.1))
+
+    def test_add_pass_positive(self):
+        self.assertEqual(3, self.calculator.add(1, 2))
+
+    def test_add_pass_negative(self):
+        self.assertEqual(-3, self.calculator.add(-1, -2))
+
+    def test_add_fail_ValueError(self):
+        self.assertEqual('Error: Cannot add values', self.calculator.add(1, "two"))
+
+    def test_subtract_pass_positive(self):
+        self.assertEqual(1, self.calculator.subtract(3, 2))
+
+    def test_subtract_pass_negative(self):
+        self.assertEqual(-1, self.calculator.subtract(-2, -1))       
+
+    def test_subtract_fail_ValueError(self):
+        self.assertEqual('Error: Cannot subtract values', self.calculator.subtract(3, "two"))
+
+    def test_multiply_pass_positive(self):
+        self.assertEqual(6, self.calculator.multiply(2, 3))
+
+    def test_multiply_pass_negative(self):
+        self.assertEqual(-2, self.calculator.multiply(-1, 2))
+
+    def test_multiply_fail_ValueError(self):
+        self.assertEqual('Error: Cannot multiply values', self.calculator.multiply(2, "three"))
+
+    def test_divide_pass_positive(self):
+        self.assertEqual(2.5, self.calculator.divide(5, 2))
+
+    def test_divide_pass_negative(self):
+        self.assertEqual(-2, self.calculator.divide(2, -1))
+
+    def test_divide_fail_ValueError(self):
+        self.assertEqual('Error: Cannot divide values', self.calculator.divide(6, "three"))
+
+    def test_divide_fail_divide_by_zero(self):
+        self.assertEqual('Error: Cannot divide by zero', self.calculator.divide(2, 0))
